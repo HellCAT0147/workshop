44c959426534013cb50df786bf4b27aa30be6ff6
Ilya <Ilya@inbox.ru>
2023-03-10 13:38:26 +0500

    check

---
+++ index.js
@@ -0,0 +1,9 @@
+const string = 'string';
+const number = 10;
+const boolean = true;
+const nullVar = null;
+const undefinedVar = undefined;
+const object = {};
+const array = [];
+// Nan - Not a Number: String / String
+console.log('Hello');
